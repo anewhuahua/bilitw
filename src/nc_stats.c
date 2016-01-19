bf406ebc1cbf37fb6ab3cdf041171748af99b7f0
huaxufan <huaxufan@bilibili.com>
2016-01-19 10:51:19 +0800

    1.1.1

--- src/nc_stats.c
+++ src/nc_stats.c
@@ -833,6 +833,7 @@ stats_master_send_rsp(int *psd)
 		log_error("set channel %d nonblock failed while core timeout %s", 
 		        nc_processes[i].channel[0] , strerror(errno));
 	}
+	shutdown(sd);
 	close(sd);
     return NC_OK;
 }
